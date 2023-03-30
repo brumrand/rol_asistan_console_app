82aeabf2cee76c0d583980e165cbda0cfa43dd1f
Bruno <brunofragag99@gmail.com>
2023-03-30 10:54:18 +0200

    specs para tiradas básicas listas

--- clases/characterDataModel.py
+++ clases/characterDataModel.py
@@ -15,5 +15,36 @@ class characterDataModel :
         print( self.skills)
         pass
     def returnThrowNecesaryData(self, data):
-        return self.skills[data]
+        name = data
+        data = self.skills[data]
+        haki = self._hakiUseLevel(data["haki"])
+        return self._attackOrThrow({"name":name, "skill": data["level"], "attr":self.attributes[data["base"]], "haki": haki, "cost": (haki*3)}, 0) 
+    
+    def _hakiUseLevel(self, type):
+        if type != "none":
+            level = self.hakis[type]
+            #print("su nivel de haki de "+type+" es "+level)
+            use = input("Cuanto haki desea usar -> ")
+            use = int(use)
+            if use > level:
+                use = level
+            elif use <= 0:
+                 use = 0
+        else :
+            use = 0
+            return use
+        
+    def _attackOrThrow (self, data, type):
+        if type==0:
+            attack = data["skill"]+ data["attr"]+ (data["haki"] *2)
+            text = "\n skill  : "+str(data["skill"])+"\n atribute : "+str(data["attr"])+"\n haki lvl : "+str(data["haki"])+"\n cost : "+str(data["cost"])+"\n "+str(data['name'])+" : "+str(attack)
+        elif type == 1 :
+            attack = data["skill"]+ data["attr"]+ data["haki"]+ data["accurancy"]
+            damage =data["damage"]+ attack
+            text = "\n skill  : "+str(data["skill"])+"\n atribute : "+str(data["attr"])+"\n haki : "+str(data["haki"])+"\n accurancy : "+str(data["accurancy"])+"\n damage : "+str(data["damage"])+"\n cost : "+str(data["cost"])+"\n attack : " +str(attack)+"--damage : "+str(damage)
+        return text
+
+    
+        
+    
     pass
